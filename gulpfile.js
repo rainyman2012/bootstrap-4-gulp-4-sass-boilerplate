8cbf784c78c03f22ae5c906db13d880eb6d55ab3
Ehsan Ahmadi <e.rainyman2012@gmail.com>
2019-07-27 04:40:18 -0700

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
+var gulp = require('gulp');
+
+
+gulp.task('sass', function() {
+    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
+        .pipe(sass())
+        .pipe(gulp.dest("src/css"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('js', function() {
+    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js',
+            'node_modules/jquery/dist/jquery.min.js',
+        ])
+        .pipe(gulp.dest('src/js'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('serve', gulp.series('sass', function() {
+    browserSync.init({
+        server: "./src"
+    });
+    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], gulp.series('sass'));
+    gulp.watch('src/*.html').on('change', browserSync.reload);
+}));
+
+gulp.task('default', gulp.series('sass', 'js', 'serve'));
